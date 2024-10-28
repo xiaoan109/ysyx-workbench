374d63c53cb1d036dd643156d86b277a8f81ed1f
ysyx <18364998790@163.com>
2024-10-28 13:28:29 +0800

    sync files

---
+++ am-kernels/kernels/bad-apple/build/native/bad-apple.d
@@ -0,0 +1,6 @@
+/home/ysyx/ysyx-workbench/am-kernels/kernels/bad-apple/build/native/bad-apple.o: \
+ /home/ysyx/ysyx-workbench/am-kernels/kernels/bad-apple/bad-apple.c \
+ /home/ysyx/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/ysyx/ysyx-workbench/abstract-machine/am/include/arch/native.h \
+ /home/ysyx/ysyx-workbench/abstract-machine/am/include/amdev.h \
+ /home/ysyx/ysyx-workbench/abstract-machine/klib/include/klib-macros.h
---
+++ am-kernels/kernels/bad-apple/build/native/resources.d
@@ -0,0 +1,2 @@
+/home/ysyx/ysyx-workbench/am-kernels/kernels/bad-apple/build/native/resources.o: \
+ /home/ysyx/ysyx-workbench/am-kernels/kernels/bad-apple/resources.S
